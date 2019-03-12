9e2c1539215d4654478609ddba27b3e9d215a966
ilhamp7 <48472174+ilhamp7@users.noreply.github.com>
2019-03-12 15:58:35 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
-    <tr>
-        <td>NIM</td>
-        <td> : </td>
-        <td>16.01.53.0064</td>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
-    </tr>
-</table> <br>";
+echo "<h2>Hello world!</h2>";
+echo "Nama : Ilhampzzz<br>";
+echo "NIM : 16.01.53.0158<br>";
 
-    echo $text;
-?>
+?> 
+<a href="https://www.unisbank.ac.id/v2/">Go To Akademik</a>
 
-    <a href="/akademik">Go To Akademik</a>
+</body>
+</html>
